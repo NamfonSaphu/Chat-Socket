d0e44562962cab6bc438c6f92a208f5e5e84b420
NamfonSaphu <namfonsaphu@gmail.com>
2025-05-09 23:52:46 +0700

    commit

--- app/api/socket.ts
+++ app/api/socket.ts
@@ -1,25 +1,14 @@
-import { Server as IOServer } from "socket.io";
-import { NextApiRequest, NextApiResponse } from "next";
-import { Server as NetServer } from "http";
-import { Socket } from "net";
+import { Server } from "socket.io";
 
-type NextApiResponseWithSocket = NextApiResponse & {
-  socket: Socket & {
-    server: NetServer & {
-      io?: IOServer;
-    };
-  };
-};
-
-export default function handler(req: NextApiRequest, res: NextApiResponseWithSocket) {
+export default function handler(req, res) {
   if (!res.socket.server.io) {
-    const io = new IOServer(res.socket.server);
+    const io = new Server(res.socket.server);
     res.socket.server.io = io;
 
-    io.on("connection", (socket) => {
+    io.on("connection", socket => {
       console.log("New user connected");
 
-      socket.on("send-message", (msg) => {
+      socket.on("send-message", msg => {
         socket.broadcast.emit("receive-message", msg);
       });
     });
